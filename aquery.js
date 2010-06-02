e531e1b8162ce957c611107c961553ddcef1f3f5
James Sulak <jsulak@gmail.com>
2010-06-02 17:24:11 -0500

    Everything but children works again

--- aquery.js
+++ aquery.js
@@ -23,33 +23,87 @@
 // The AQuery object inherits from the array object
 // TODO: Later, we may want to explicitly say which methods we want to keep
 
-var aQueryCreator = function(document) {
-   // Define AQuery as an array so we get all the methods
-   var AQuery = {};
-   AQuery.push = Array.prototype.push;
-   AQuery.slice = Array.prototype.slice;
-   AQuery.forEach = Array.prototype.forEach;
-   AQuery.map = Array.prototype.map;
-   AQuery.indexOf = Array.prototype.indexOf;
-
-   AQuery.remove = function() {
-      this.forEach(function(elem) {
-		      var parent = elem.getParentNode();
-		      parent.removeChild(elem);
-		   });
+var _$ = function(document) {
+   var aQuery = function(selector, context) {
+      // The aQuery object is actually just the init constructor "enhanced."
+      return new aQuery.fn.init(selector, context);
    };
 
-   AQuery.attr = function(name, value) {
-      if (value == null) {
-	 return this[0].getAttribute(name);
-      }
-      else {
-	 this.forEach(function(elem) { elem.setAttribute(name, value); });
-	 return this;
-      }
+   aQuery.fn = aQuery.prototype = {
+
+      init : function(selector, context) {
+	 // Handle $(""), $(null), or $(undefined)
+	 if (!selector) {
+	    return this;
+	 }
+
+	 // If it starts with "#", then we want to match ids
+	 if (selector.match(/^#/)) {
+	    var id = selector.substr(1);
+	    return document.getElementById(id);
+	 }
+
+	 // If it is a valid xpath expression, then do that
+	 else if (selector.indexOf("/") != -1 && Acl.func("xpath_valid", selector)) {
+	    // TODO: later implement this more natively
+
+	    var oidNodesString = Acl.func("jmp_utils::get_doc_xpath_oids",
+					  selector,
+					  Acl.func("current_doc"));
+	    var oids = oidNodesString.split("-");
+	    var result = aQuery();
+	    for (var i = 0; i < oids.length; i++ ) {
+	       result.push(Acl.DOMOID(oids[i]));
+	    }
+	    return result;
+	 }
+
+	 // Otherwise return all elements in document with the
+	 // provided name
+	 else {
+	    var nodes = document.getElementsByTagName(selector);
+	    var result = aQuery();
+	    for (var i = 0; i < nodes.length; i++) {
+	       result.push(nodes.item(i));
+	    }
+	    return result;
+	 }
+      },
+
+      remove : function() {
+	 this.forEach(function(elem) {
+			 var parent = elem.getParentNode();
+			 parent.removeChild(elem);
+		      });
+      },
+
+      attr : function(name, value) {
+	 if (value == null) {
+	    return this[0].getAttribute(name);
+	 }
+	 else {
+	    this.forEach(function(elem) { elem.setAttribute(name, value); });
+	    return this;
+	 }
+      },
+
+
+      // Make array methods avaliable
+      // TODO: later do this in some more automated way
+
+      push : Array.prototype.push,
+      slice : Array.prototype.slice,
+      forEach : Array.prototype.forEach,
+      map : Array.prototype.map,
+      indexOf : Array.prototype.indexOf
    };
 
-   return AQuery;
+   //aQuery.prototype.howdy = "aloha";
+
+   // Give the init function the aQuery prototype for later instantiation
+   aQuery.fn.init.prototype = aQuery.fn;
+
+   return aQuery;
 };
 
 
@@ -57,7 +111,7 @@ var aQueryCreator = function(document) {
 // TODO: Fix this.  This is a total hack. Ideally,
 // there would be one implementation, that functioned
 // only on the array.
-
+/*
 var _$ = function(document) {
 
    var _element = function(elem) {
@@ -75,37 +129,4 @@ var _$ = function(document) {
       };
    };
 
-   return function(term) {
-      // If it starts with "#", then we want to match ids
-      if (term.match(/^#/)) {
-	 var id = term.substr(1);
-	 return document.getElementById(id);
-      }
-
-      // If it is a valid xpath expression, then do that
-      else if (term.indexOf("/") != -1 && Acl.func("xpath_valid", term)) {
-	 // TODO: later implement this more natively
-
-	 var oidNodesString = Acl.func("jmp_utils::get_doc_xpath_oids",
-				       term,
-				       Acl.func("current_doc"));
-	 var oids = oidNodesString.split("-");
-	 var result = aQueryCreator();
-	 for (var i = 0; i < oids.length; i++ ) {
-	    result.push(Acl.DOMOID(oids[i]));
-	 }
-	 return result;
-      }
-
-      // Otherwise return all elements in document with the
-      // provided name
-      else {
-	 var nodes = document.getElementsByTagName(term);
-	 var result = aQueryCreator();
-	 for (var i = 0; i < nodes.length; i++) {
-	    result.push(nodes.item(i));
-	 }
-	 return result;
-      }
-   };
-};
+*/
