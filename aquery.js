feec8bdbe65988b0b56306f00122a7007391b4a3
James Sulak <jsulak@gmail.com>
2011-01-12 07:46:58 -0600

    added aQuery.extend

--- aquery.js
+++ aquery.js
@@ -337,6 +337,72 @@ var _$ = function(document) {
    aQuery.fn.init.prototype = aQuery.fn;
 
 
+   aQuery.extend = aQuery.fn.extend = function() {
+      var options, name, src, copy, copyIsArray, clone,
+          target = arguments[0] || {},
+	  i = 1,
+	  length = arguments.length,
+	  deep = false;
+
+      // Handle a deep copy situation
+      if ( typeof target === "boolean" ) {
+	 deep = target;
+	 target = arguments[1] || {};
+	 // skip the boolean and the target
+	 i = 2;
+      }
+
+      // Handle case when target is a string or something (possible in deep copy)
+      if ( typeof target !== "object" && !aQuery.isFunction(target) ) {
+	 target = {};
+      }
+
+      // extend aQuery itself if only one argument is passed
+      if ( length === i ) {
+	 target = this;
+	 --i;
+      }
+
+      for ( ; i < length; i++ ) {
+	 // Only deal with non-null/undefined values
+	 if ( (options = arguments[ i ]) != null ) {
+	    // Extend the base object
+	    for ( name in options ) {
+	       src = target[ name ];
+	       copy = options[ name ];
+
+	       // Prevent never-ending loop
+	       if ( target === copy ) {
+		  continue;
+	       }
+
+	       // Recurse if we're merging plain objects or arrays
+	       if ( deep && copy && ( aQuery.isPlainObject(copy) || (copyIsArray = aQuery.isArray(copy)) ) ) {
+		  if ( copyIsArray ) {
+		     copyIsArray = false;
+		     clone = src && aQuery.isArray(src) ? src : [];
+
+		  } else {
+		     clone = src && aQuery.isPlainObject(src) ? src : {};
+		  }
+
+		  // Never move original objects, clone them
+		  target[ name ] = aQuery.extend( deep, clone, copy );
+
+		  // Don't bring in undefined values
+	       } else if ( copy !== undefined ) {
+		  target[ name ] = copy;
+	       }
+	    }
+	 }
+      }
+
+      // Return the modified object
+      return target;
+   };
+
+
+
 
    // args is for internal usage only
    aQuery.each = function( object, callback, args ) {
--- test/test.js
+++ test/test.js
@@ -415,6 +415,109 @@ var aQueryTests = function() {
 	      same( parse({length:0}, [1,2]), {length:2, 0:1, 1:2}, "First array like");
 	   });
 
+      test("aQuery.extend(Object, Object)", function() {
+	      expect(27);
+
+	      var settings = { xnumber1: 5, xnumber2: 7, xstring1: "peter", xstring2: "pan" },
+	      options = { xnumber2: 1, xstring2: "x", xxx: "newstring" },
+	      optionsCopy = { xnumber2: 1, xstring2: "x", xxx: "newstring" },
+	      merged = { xnumber1: 5, xnumber2: 1, xstring1: "peter", xstring2: "x", xxx: "newstring" },
+	      deep1 = { foo: { bar: true } },
+	      deep1copy = { foo: { bar: true } },
+	      deep2 = { foo: { baz: true }, foo2: document },
+	      deep2copy = { foo: { baz: true }, foo2: document },
+	      deepmerged = { foo: { bar: true, baz: true }, foo2: document },
+	      arr = [1, 2, 3],
+	      nestedarray = { arr: arr };
+
+	      aQuery.extend(settings, options);
+	      same( settings, merged, "Check if extended: settings must be extended" );
+	      same( options, optionsCopy, "Check if not modified: options must not be modified" );
+
+	      aQuery.extend(settings, null, options);
+	      same( settings, merged, "Check if extended: settings must be extended" );
+	      same( options, optionsCopy, "Check if not modified: options must not be modified" );
+
+	      aQuery.extend(true, deep1, deep2);
+	      same( deep1.foo, deepmerged.foo, "Check if foo: settings must be extended" );
+	      same( deep2.foo, deep2copy.foo, "Check if not deep2: options must not be modified" );
+	      equals( deep1.foo2, document, "Make sure that a deep clone was not attempted on the document" );
+
+	      ok( aQuery.extend(true, [], arr) !== arr, "Deep extend of array must clone array" );
+	      ok( aQuery.extend(true, {}, nestedarray).arr !== arr, "Deep extend of object must clone child array" );
+
+	      var empty = {};
+	      var optionsWithLength = { foo: { length: -1 } };
+	      aQuery.extend(true, empty, optionsWithLength);
+	      same( empty.foo, optionsWithLength.foo, "The length property must copy correctly" );
+
+	      empty = {};
+	      var optionsWithDate = { foo: { date: new Date } };
+	      aQuery.extend(true, empty, optionsWithDate);
+	      same( empty.foo, optionsWithDate.foo, "Dates copy correctly" );
+
+	      var myKlass = function() {};
+	      var customObject = new myKlass();
+	      var optionsWithCustomObject = { foo: { date: customObject } };
+	      empty = {};
+	      aQuery.extend(true, empty, optionsWithCustomObject);
+	      ok( empty.foo && empty.foo.date === customObject, "Custom objects copy correctly (no methods)" );
+
+	      // Makes the class a little more realistic
+	      myKlass.prototype = { someMethod: function(){} };
+	      empty = {};
+	      aQuery.extend(true, empty, optionsWithCustomObject);
+	      ok( empty.foo && empty.foo.date === customObject, "Custom objects copy correctly" );
+
+	      var ret = aQuery.extend(true, { foo: 4 }, { foo: new Number(5) } );
+	      ok( ret.foo == 5, "Wrapped numbers copy correctly" );
+
+	      var nullUndef;
+	      nullUndef = aQuery.extend({}, options, { xnumber2: null });
+	      ok( nullUndef.xnumber2 === null, "Check to make sure null values are copied");
+
+	      nullUndef = aQuery.extend({}, options, { xnumber2: undefined });
+	      ok( nullUndef.xnumber2 === options.xnumber2, "Check to make sure undefined values are not copied");
+
+	      nullUndef = aQuery.extend({}, options, { xnumber0: null });
+	      ok( nullUndef.xnumber0 === null, "Check to make sure null values are inserted");
+
+	      var target = {};
+	      var recursive = { foo:target, bar:5 };
+	      aQuery.extend(true, target, recursive);
+	      same( target, { bar:5 }, "Check to make sure a recursive obj doesn't go never-ending loop by not copying it over" );
+
+	      var ret = aQuery.extend(true, { foo: [] }, { foo: [0] } ); // 1907
+	      equals( ret.foo.length, 1, "Check to make sure a value with coersion 'false' copies over when necessary to fix #1907" );
+
+	      var ret = aQuery.extend(true, { foo: "1,2,3" }, { foo: [1, 2, 3] } );
+	      ok( typeof ret.foo != "string", "Check to make sure values equal with coersion (but not actually equal) overwrite correctly" );
+
+	      var ret = aQuery.extend(true, { foo:"bar" }, { foo:null } );
+	      ok( typeof ret.foo !== 'undefined', "Make sure a null value doesn't crash with deep extend, for #1908" );
+
+	      var obj = { foo:null };
+	      aQuery.extend(true, obj, { foo:"notnull" } );
+	      equals( obj.foo, "notnull", "Make sure a null value can be overwritten" );
+
+	      function func() {}
+	      aQuery.extend(func, { key: "value" } );
+	      equals( func.key, "value", "Verify a function can be extended" );
+
+	      var defaults = { xnumber1: 5, xnumber2: 7, xstring1: "peter", xstring2: "pan" },
+	      defaultsCopy = { xnumber1: 5, xnumber2: 7, xstring1: "peter", xstring2: "pan" },
+	      options1 = { xnumber2: 1, xstring2: "x" },
+	      options1Copy = { xnumber2: 1, xstring2: "x" },
+	      options2 = { xstring2: "xx", xxx: "newstringx" },
+	      options2Copy = { xstring2: "xx", xxx: "newstringx" },
+	      merged2 = { xnumber1: 5, xnumber2: 1, xstring1: "peter", xstring2: "xx", xxx: "newstringx" };
+
+	      var settings = aQuery.extend({}, defaults, options1, options2);
+	      same( settings, merged2, "Check if extended: settings must be extended" );
+	      same( defaults, defaultsCopy, "Check if not modified: options1 must not be modified" );
+	      same( options1, options1Copy, "Check if not modified: options1 must not be modified" );
+	      same( options2, options2Copy, "Check if not modified: options2 must not be modified" );
+	   });
 
 
 
